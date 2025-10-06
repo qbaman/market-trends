907dd2bd28a3429375496383549bdfb4771396a0
qbaman <78668489+qbaman@users.noreply.github.com>
2025-10-06 22:04:29 +0100

    chore: initial import of MarketAnalyzerApp

---
+++ MarketAnalyzerApp/ProductsForm.cs
@@ -0,0 +1,127 @@
+using MarketAnalyzerApp.Data;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MarketAnalyzerApp;
+
+public partial class ProductsForm : Form
+{
+    public ProductsForm()
+    {
+        InitializeComponent();
+    }
+    List<Product> _products;
+    private void ProductsForm_Load(object sender, EventArgs e)
+    {
+        CustomizeGridAppearance();
+        _products = CSVDataLoader.LoadData();
+        RefreshGrid();
+
+        ConfigureChart();
+        PricesChart.Visible = false;
+    }
+    private void CustomizeGridAppearance()
+    {
+        ProductsGrid.AutoGenerateColumns = false;
+        ProductsGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        ProductsGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        ProductsGrid.RowHeadersVisible = false;
+
+        DataGridViewTextBoxColumn titleCol = new DataGridViewTextBoxColumn();
+        titleCol.HeaderText = "Title";
+        titleCol.DataPropertyName = "Title";
+        ProductsGrid.Columns.Add(titleCol);
+
+        DataGridViewTextBoxColumn brandCol = new DataGridViewTextBoxColumn();
+        brandCol.HeaderText = "Brand";
+        brandCol.DataPropertyName = "Brand";
+        ProductsGrid.Columns.Add(brandCol);
+    }
+    private async void SearchText_TextChanged(object sender, EventArgs e)
+    {
+        int lengthBeforePause = SearchText.Text.Length;
+
+        await Task.Delay(300);
+
+        int lengthAfterPause = SearchText.Text.Length;
+
+        if (lengthBeforePause == lengthAfterPause)
+            RefreshGrid();
+    }
+    private void RefreshGrid()
+    {
+        List<Product> searchResult = _products.Where(p => p.Brand.ToLower().Contains(SearchText.Text.ToLower())).ToList();
+
+        ProductsGrid.DataSource = searchResult;
+    }
+    private void RefreshChart(Product selectedProduct)
+    {
+        PricesChart.Visible = true;
+
+        PricesChart.Series["price"].Points.Clear();
+        PricesChart.Series["price"].Points.AddXY("dec 2021", Calculations.ConvertToDecimal(selectedProduct.Dec2021Price));
+        PricesChart.Series["rice"].Points.AddXY("may 2022", Calculations.ConvertToDecimal(selectedProduct.May2022Price));
+        PricesChart.Series["price"].Points.AddXY("jul 2022", Calculations.ConvertToDecimal(selectedProduct.Jul2022Price));
+        PricesChart.Series["price"].Points.AddXY("sep 2022", Calculations.ConvertToDecimal(selectedProduct.Sep2022Price));
+        PricesChart.Series["price"].Points.AddXY("oct 2022", Calculations.ConvertToDecimal(selectedProduct.Oct2022Price));
+
+        foreach (DataPoint point in PricesChart.Series["price"].Points)
+            point.Label = point.YValues[0].ToString();
+    }
+    private void ConfigureChart()
+    {
+        PricesChart.Series.Clear();
+        PricesChart.ChartAreas.Clear();
+
+        PricesChart.ChartAreas.Add(new ChartArea());
+        PricesChart.Series.Add(new Series("price")
+        {
+            ChartType = SeriesChartType.Area
+        });
+
+    }
+    private void ProductsGrid_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+    {
+        Product selectedProduct = (Product)ProductsGrid.Rows[e.RowIndex].DataBoundItem;
+        RefreshChart(selectedProduct);
+    }
+    private void CalculateChangeButton_Click(object sender, EventArgs e)
+    {
+        var selectedRows = ProductsGrid.SelectedRows;
+        if (selectedRows.Count == 0)
+            return;
+
+        List<Product> selectedProducts = new List<Product>();
+
+        foreach (DataGridViewRow row in selectedRows)
+        {
+            selectedProducts.Add((Product)row.DataBoundItem);
+        }
+
+        string message = Calculations.CalculateChangeInPrice(selectedProducts);
+        MessageBox.Show(message);
+    }
+    private void BiggestIncreaseBtn_Click(object sender, EventArgs e)
+    {
+        Dictionary<string, decimal> brandsWithHighestPriceIncrease = Calculations.CalculateBrandsPriceChange(_products, SortType.Increase);
+
+        string message = "";
+        foreach (var brand in brandsWithHighestPriceIncrease)
+        {
+            message += $"{brand.Key} increased price for {brand.Value}%\n\n";
+        }
+
+        MessageBox.Show(message, "top brands with highest price increase between dec 2021 and oct 2022");
+    }
+    private void BiggestDecreaseBtn_Click(object sender, EventArgs e)
+    {
+        Dictionary<string, decimal> brandsWithHighestPriceIncrease = Calculations.CalculateBrandsPriceChange(_products, SortType.Decrease);
+
+        string message = "";
+        foreach (var brand in brandsWithHighestPriceIncrease)
+        {
+            message += $"{brand.Key} dropped price for {brand.Value}%\n\n";
+        }
+
+        MessageBox.Show(message, "top brands with highest price decrease between dec 2021 and oct 2022");
+    }
+}
